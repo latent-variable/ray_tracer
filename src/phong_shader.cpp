46bdbc047000f1ca445bd4e9d665086e3144b9e2
lgonz041 <lgonz041@ucr.edu>
2018-01-26 11:48:32 -0800

    almost working specular

--- src/phong_shader.cpp
+++ src/phong_shader.cpp
@@ -35,19 +35,20 @@ Shade_Surface(const Ray& ray,const vec3& intersection_point,
 
           //Diffuse Id = Rd*Ld*max(0,light*normal)
           float cs = max( dot((L-I).normalized(),N), 0.0);
-          //if(cs){
-             //square distance between the intersection point and the light source
+          //square distance between the intersection point and the light source
+          Diffuse = color_diffuse  * cs * light_color;
 
-             Diffuse = color_diffuse  * cs * light_color;
-          //}else  Diffuse = {0,0,0};
 
           //Specular
           vec3 L_norm = L.normalized();
           vec3 R = ( 2.0* dot(L_norm,N)* N - L_norm);
+          //vec3 R = ( L - 2.0* dot(L_norm,same_side_normal)* same_side_normal );
           R = R.normalized();
-          float Specular_intensity = pow(max(dot( R , world.camera.position.normalized()), 0.0),specular_power);
-          cout << "Specular " << Specular_intensity <<endl;
-          Specular = color_specular * light_color * Specular_intensity;
+          float cs2 = max(dot( R , world.camera.position.normalized()),0.0);
+          double Specular_intensity = pow(cs2,specular_power);
+          //cout << "Specular " << Specular_intensity <<endl;
+          //cout << "color_specular "<< color_specular.normalized()<<endl;
+          Specular = (light_color*color_specular)*Specular_intensity;
 
           color = color + Diffuse + Specular;
     }

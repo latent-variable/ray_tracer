395f3bea59a2bf790807d39b024a5b1a4ebb9717
lgonz041 <lgonz041@ucr.edu>
2018-01-26 16:42:34 -0800

    checkpoint 2 done

--- src/phong_shader.cpp
+++ src/phong_shader.cpp
@@ -14,7 +14,7 @@ Shade_Surface(const Ray& ray,const vec3& intersection_point,
 {
     vec3 color;
     vec3 I = intersection_point;
-    vec3 N = same_side_normal.normalized();
+    vec3 N = same_side_normal;
 
     vec3 Ambient;
     vec3 Diffuse;
@@ -28,30 +28,48 @@ Shade_Surface(const Ray& ray,const vec3& intersection_point,
      color = Ambient;
 
      for(unsigned i = 0; i < world.lights.size(); i++){
-          vec3 L = world.lights[i]->position;
-          float d = dot((I-L),(I-L));
-          vec3 light_color = ((world.lights[i]->Emitted_Light(ray))/ d);
+
+        vec3 L = world.lights[i]->position - I ; // Light vector to intersect
+        float d = dot((L),(L)); // Magnitude of Light Square
+
+        if (world.enable_shadows){
+
+           Hit hit;
+           Ray ray2(I,L.normalized());
+           Object *obj = world.Closest_Intersection(ray2,hit );
+
+           if(!obj){
+                goto COLOR;
+            }else{
+                if(hit.t < sqrt(d) )
+                    continue;
+                else goto COLOR;
+
+            }
+        }else{
+
+           COLOR:
+
+           //Light color square distance between the intersection point and the light source
+           vec3 light_color = ((world.lights[i]->Emitted_Light(ray))/ d);
+
+           //Diffuse Id = Rd*Ld*max(0,light*normal)
+           float cs = max( dot((L).normalized(),N), 0.0);
+           //square distance between the intersection point and the light source
+           Diffuse = color_diffuse  * cs * light_color;
+
+           //Specular
+           vec3 L_norm = L.normalized();
+           vec3 R = ( 2.0* dot(L_norm,N)* N - L_norm);
+           R = R.normalized();
+           float cs2 = max(dot( R , (-ray.direction).normalized() ),0.0);
+           double Specular_intensity = pow(cs2,specular_power);
+           Specular = (light_color*color_specular)*Specular_intensity;
+
+           color = color + Diffuse + Specular;
+       }
 
 
-          //Diffuse Id = Rd*Ld*max(0,light*normal)
-          float cs = max( dot((L-I).normalized(),N), 0.0);
-          //square distance between the intersection point and the light source
-          Diffuse = color_diffuse  * cs * light_color;
-
-
-          //Specular
-          vec3 L_norm = L.normalized();
-          vec3 R = ( 2.0* dot(L_norm,N)* N - L_norm);
-          //vec3 R = ( L - 2.0* dot(L_norm,same_side_normal)* same_side_normal );
-          R = R.normalized();
-          float cs2 = max(dot( R , world.camera.position.normalized()),0.0);
-          double Specular_intensity = pow(cs2,specular_power);
-          //cout << "Specular " << Specular_intensity <<endl;
-          //cout << "color_specular "<< color_specular.normalized()<<endl;
-          Specular = (light_color*color_specular)*Specular_intensity;
-
-          color = color + Diffuse + Specular;
-    }
-
+     }
     return color;
 }
